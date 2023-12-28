3c1d4d41edf8db760ea9fd68a0ca92e9f0f1678c
raineymarkr <raineymarkr@protonmail.com>
2023-12-27 19:19:11 -0600

    school menu auto updates

--- site/src/components/SchoolMenu.js
+++ site/src/components/SchoolMenu.js
@@ -2,17 +2,35 @@ import React, { useState, useEffect } from 'react';
 import './SchoolMenu.css'; // Import the CSS file
 
 const SchoolMenu = () => {
-  // Get the current date and format it to yyyy-mm-dd
-  const date = new Date();
-  // Format the current date to yyyy-mm-dd
-  const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1)
-    .toString()
-    .padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+  const [date, setDate] = useState(new Date());
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      // Update the date to the current date
+      setDate(new Date());
+    }, 86400000); // Update date every 24 hours (adjust as needed)
 
-  // Construct file paths for today's menu
-  const breakfastImagePath = `./breakfast/${formattedDate}.png`;
-  const lunchImagePath = `./lunch/${formattedDate}.png`;
+    // Initial menu load
+    fetchMenu();
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const fetchMenu = () => {
+    // Format the current date to yyyy-mm-dd
+    const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1)
+      .toString()
+      .padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+
+    // Construct file paths for today's menu
+    const breakfastImagePath = `./breakfast/${formattedDate}.png`;
+    const lunchImagePath = `./lunch/${formattedDate}.png`;
+
+    // Fetch the menu images or perform any necessary rendering
+    // Example: you can set these paths in the state or render them directly
+    console.log('Breakfast Menu Path:', breakfastImagePath);
+    console.log('Lunch Menu Path:', lunchImagePath);
+  };
 
   return (
     <div>
@@ -21,13 +39,13 @@ const SchoolMenu = () => {
           <center>
             <h3>Breakfast</h3>
           </center>
-          <img src={breakfastImagePath} className="img-fluid p-4" alt="Breakfast Menu" />
+          {/* You can render the menu images here if needed */}
         </div>
         <div>
           <center>
             <h3>Lunch</h3>
           </center>
-          <img src={lunchImagePath} className="img-fluid p-4" alt="Lunch Menu" />
+          {/* You can render the menu images here if needed */}
         </div>
       </div>
     </div>
